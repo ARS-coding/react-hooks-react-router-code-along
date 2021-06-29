662299040f771400859b950b72f4064b0c5b9ee6
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-29 13:43:56 +0300

    Import BrowserRouter from browser-router-dom package and set the path of Home component to exactly '/'.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
 
 function Home() {
   return (
@@ -9,4 +11,12 @@ function Home() {
   );
 }
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <Route exact path="/">
+      <Home />
+    </Route>
+
+
+  </Router>
+, document.getElementById("root"));
